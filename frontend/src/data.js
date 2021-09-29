75ae65dfa837503ccde170677127ad57c42ffce1
pedro-quila <kila1213@gmail.com>
2021-09-28 21:00:15 -0300

    git-comercio-1

---
+++ frontend/src/data.js
@@ -0,0 +1,70 @@
+export default {
+    products: [
+        {
+            _id: '1',
+            name: 'producto 1',
+            category: 'categoria 1',
+            image: '/images/product-1.jpg',
+            price: 50,
+            brand: 'marca 1',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '2',
+            name: 'producto 2',
+            category: 'categoria 2',
+            image: '/images/product-2.jpg',
+            price: 50,
+            brand: 'marca 2',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '3',
+            name: 'producto 3',
+            category: 'categoria 3',
+            image: '/images/product-3.jpg',
+            price: 50,
+            brand: 'marca 3',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '4',
+            name: 'producto 1',
+            category: 'categoria 4',
+            image: '/images/product-4.jpg',
+            price: 50,
+            brand: 'marca 4',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '5',
+            name: 'producto 5',
+            category: 'categoria 5',
+            image: '/images/product-5.jpg',
+            price: 50,
+            brand: 'marca 5',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+        {
+            _id: '6',
+            name: 'producto 6',
+            category: 'categoria 6',
+            image: '/images/product-6.jpg',
+            price: 50,
+            brand: 'marca 6',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 6
+        },
+    ]
+}
